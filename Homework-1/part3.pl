e30d1050be7419796eb4ff8008ab2fba7aa6fabf
MuratovKl <muratovkirill@outlook.com>
2016-09-30 00:35:49 +0400

    Create part3.pl

---
+++ Homework-1/part3.pl
@@ -0,0 +1,15 @@
+use strict;
+use warnings;
+use Data::Dumper;
+
+my @structure;
+my $i=0;
+open(my $myfile, "<", "out.ref" )
+or die "Can't open file $!";
+while(<$myfile>){
+my @unit= ();
+chomp;
+@unit= split(/;/,$_);
+@structure[$i++]= \@unit;
+}
+print Dumper(@structure);
