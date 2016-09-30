98fc1d3513a1ade48be026d9aa04b03f8be4a40b
MuratovKl <muratovkirill@outlook.com>
2016-09-30 14:18:43 +0400

    Update part3.pl

--- Homework-1/part3.pl
+++ Homework-1/part3.pl
@@ -4,8 +4,8 @@ use Data::Dumper;
 
 my @structure;
 my $i=0;
-open(my $myfile, "<", "out.ref" )
-or die "Can't open file $!";
+open(my $myfile, "<", "out.txt" )
+or die "Can't open file! $!";
 while(<$myfile>){
 my @unit= ();
 chomp;
