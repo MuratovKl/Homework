55bae4169e7163dc011706d8eaa53c3278c32b2f
MuratovKl <muratovkirill@outlook.com>
2016-12-15 21:41:34 +0300

    Update JSON.pm

--- oop_reducer/lib/Local/Row/JSON.pm
+++ oop_reducer/lib/Local/Row/JSON.pm
@@ -5,9 +5,9 @@ use warnings;
 use JSON::XS;
 use parent 'Local::Row';
 
-	sub parse {
-		my ($self, $str) = @_;
-		return JSON::XS -> new -> utf8 -> decode($str);
-	}
+sub parse {
+	my ($self, $str) = @_;
+	return JSON::XS -> new -> utf8 -> decode($str);
+}
 
 1;
